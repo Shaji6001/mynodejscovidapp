b2e99c5da38ac0879cc665b3c89db185102d5dff
Shaji6001 <shajishaijigmail.com>
2021-02-25 22:29:32 +0300

    update

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 mongoose.connect("mongodb+srv://shaji:ponnu123@cluster1.u2cuq.mongodb.net/coviddb?retryWrites=true&w=majority",{ useUnifiedTopology: true },{ useNewUrlParser: true })
 
+
 app.post('/add', async(req,res)=>{
 
     try
@@ -19,27 +20,30 @@ app.post('/add', async(req,res)=>{
       var patientData= new patientModel(data);
 
       var result = await patientData.save();
+      console.log(result);
       res.json(result);
 
     }
    catch(error)
    {
-
+        res.status(500).send(error)
    }
 })
 
-app.get('/viewall',async(req,res)=>{
 
-      try
-    {
-       var result = await patientModel.find().exec();
-       res.json(result);
-    }
-       catch(error)
-   {
-       res.status(500).send(error)
-   }
+
+app.get('/viewall', async(req, res)=>{
+  try
+  {
+    var result = await patientModel.find().exec();
+    res.json(result)
+  }
+  catch(error){
+    res.status(500).send(error);
+
+  }
 })
+
 
  app.post('/search',async(req,res)=>{
    
@@ -61,11 +65,43 @@ app.get('/viewall',async(req,res)=>{
  })
 
 
+ app.post('/delete',async(req,res)=>{
+   try
+   {
+     patientModel.findByIdAndDelete(req.body.id,(error,data)=>{
+       if(error){res.send(error)}
+
+       else {res.json({'status':'Success'})};
+     })
+
+     
+   }
+   catch(error){res.status(500).send(error)}
+ })
+
+ 
+ app.post('/update', async(req, res)=>{
+   try
+   {
+     patientModel.findByIdAndUpdate(req.body.id,
+      {patientName:req.body.patientName,patientAddress:req.body.patientAddress,
+      doctorName:req.body.doctorName,symtoms:req.body.symtoms,
+      patientPhoneno:req.body.patientPhoneno,emailID:req.body.emailID,patientCode:req.body.patientCode},
+      (error, data)=>{
+        if(error){throw error}
+        else{res.json({'status':"success"})};
+      })
+   }
+   catch(error){res.status(500).send(error)}
+
+ })
 
 
 
 
-app.listen(process.env.PORT || 3000,function(){
+
+
+app.listen(process.env.PORT || 3002,function(){
     console.log("Your Server is Running")
 }
 
